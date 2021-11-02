13d40a9a4e906e1bb1c929cdd4844f054cd73a45
woong99 <dndry1072@naver.com>
2021-11-02 20:53:58 +0900

    Permission

---
+++ Java_Study/SamplePermission2/app/src/main/java/org/techtown/permission2/MainActivity.java
@@ -0,0 +1,37 @@
+package org.techtown.permission2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    @Override
+    public void onDenied(int i, String[] strings) {
+        Toast.makeText(this, "permissions denied : " + strings.length, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGranted(int i, String[] strings) {
+        Toast.makeText(this, "permissions granted : " + strings.length, Toast.LENGTH_LONG).show();
+    }
+}
