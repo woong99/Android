b8cefff4bf97ff92f7f1d5bc1c19afc58e017fa0
woong99 <dndry1072@naver.com>
2021-11-03 11:57:27 +0900

    ButtonView

---
+++ Java_Study/Ch.07/SampleView/app/src/main/java/org/techtown/view/MyButton.java
@@ -0,0 +1,60 @@
+package org.techtown.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class MyButton extends AppCompatButton {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("MyButton", "onTouchEvent 호출됨");
+
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                setBackgroundColor(Color.BLUE);
+                setTextColor(Color.RED);
+                break;
+            case MotionEvent.ACTION_OUTSIDE:
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                setBackgroundColor(Color.CYAN);
+                setTextColor(Color.BLACK);
+                break;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.d("MyButton", "onDraw 호출됨");
+    }
+
+    public MyButton(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MyButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        setBackgroundColor(Color.CYAN);
+        setTextColor(Color.BLACK);
+
+        float textSize = getResources().getDimension(R.dimen.text_size);
+        setTextSize(textSize);
+    }
+}
